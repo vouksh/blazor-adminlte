975ac97c39ec4e54bd90023574cb85977429dda0
Jack Butler <JButler@drbsystems.com>
2021-04-22 16:05:20 -0400

    Customized it a bit more

---
+++ src/Blazor.AdminLte/AdminLTEOptions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Options;
+
+namespace Blazor.AdminLte
+{
+	public class AdminLTEOptions
+	{
+
+		private readonly IServiceProvider serviceProvider;
+		private readonly Action<AdminLTEOptions> configureOptions;
+
+		public AdminLTEOptions(IServiceProvider serviceProvider, Action<AdminLTEOptions> configureOptions)
+		{
+			this.serviceProvider = serviceProvider;
+			this.configureOptions = configureOptions;
+
+			this.configureOptions?.Invoke(this);
+		}
+
+		/// <summary>
+		/// If true, enables dark-mode site-wide. 
+		/// </summary>
+		public bool DarkModeEnabled { get; set; }
+
+		public IServiceProvider Services => serviceProvider;
+	}
+}
